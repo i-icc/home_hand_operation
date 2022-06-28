e6fae7106852b01e4085a9e9a474015cbaa95531
i-icc <ichi623110@gmail.com>
2022-06-28 17:59:12 +0900

    arg

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 import cv2
 from hand import HandDetector
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--draw', action="store_true",
+                    help="draw hand")
+parser.add_argument('-cam', '--camera', type=int,
+                    help="Use camera number")
+args = parser.parse_args()
 
 def main():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(int(args.camera))
 
     handDetector = HandDetector()
 
     while True:
         ret, frame = cap.read()
 
-        handDetector.findHands(frame)
-        # handDetector.findPosition(frame)
-        print(handDetector.find_pose(frame))
+        handDetector.findHands(frame, args.draw)
+        print(handDetector.find_pose(frame, args.draw))
 
         # cv2.imshow('camera' , frame)
 
